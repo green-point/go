438ba6715bc7f24d3776d6297458a074c1cbda0d
Leo <newleo@163.com>
2018-03-11 00:22:35 +0800

    add interface function

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,25 @@
 package main
 
 import (
+	"os"
+	"reflect"
+	"strconv"
 	"fmt"
 	"basic/stringutil"
 )
 
 func main() {
 	fmt.Println("Hello World")
-	fmt.Printf(stringutil.Reverse("oG!, olleH"))
+	fmt.Printf(stringutil.Reverse("!oG, olleH"))
+}
+
+func myPrint(args ...interface{}) {
+	for _, arg := range args {
+		switch v := reflect.ValueOf(arg); v.Kind() {
+		case reflect.String:
+			os.Stdout.WriteString(v.String())
+		case reflect.Int:
+			os.Stdout.WriteString(strconv.FormatInt(v.Int(), 10))
+		}
+	}
 }
